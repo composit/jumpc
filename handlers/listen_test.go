b62086d44bf44b780f4f1552cac39a3ac9b6341b
Mr. Matt <matt@composition9.com>
2017-12-11 23:20:27 +0000

    test the server listening and returning

---
+++ handlers/listen_test.go
@@ -0,0 +1,30 @@
+package handlers_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/composit/jumpc/handlers"
+)
+
+func TestListen(t *testing.T) {
+	req, err := http.NewRequest("POST", "/", strings.NewReader("password=hunter2"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rec := httptest.NewRecorder()
+	handler := http.HandlerFunc(handlers.PwdHash)
+
+	handler.ServeHTTP(rec, req)
+
+	if c := rec.Code; c != http.StatusOK {
+		t.Fatalf("server returned a bad response code: %d\n", c)
+	}
+
+	if hsh := rec.Body.String(); hsh != "abc123" {
+		t.Errorf("server returned a bad hash: %s\n", hsh)
+	}
+}
