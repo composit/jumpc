2a4df2505b25e3d25028c4b53ee9da4a9e2c0d15
Mr. Matt <matt@composition9.com>
2017-12-12 23:37:30 +0000

    remove unnecessary byte wrapper

--- handlers/listen.go
+++ handlers/listen.go
@@ -35,7 +35,7 @@ func PwdHash(w http.ResponseWriter, req *http.Request) {
 
 	h := encode.Hash(pwd)
 	b := encode.Base64(h)
-	if _, err = w.Write([]byte(b)); err != nil {
+	if _, err = w.Write(b); err != nil {
 		w.WriteHeader(500)
 		return
 	}
